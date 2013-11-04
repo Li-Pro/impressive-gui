d7b8ee331e9ac9aab8751b1a48d68db563c7ba79
keyj <keyj@58003caa-664d-4c56-a378-6d187ff66ee1>
2013-11-04 21:03:49 +0000

    Win32 version information now includes the SVN revision number
    git-svn-id: https://svn.emphy.de/impressive/trunk/impressive@69 58003caa-664d-4c56-a378-6d187ff66ee1

--- win32/get_version_info.py
+++ win32/get_version_info.py
@@ -1,14 +1,29 @@
 #!/usr/bin/head -n 2
 # This tool is useful on Win32 only.
-import sys, re
+import sys, re, subprocess
 
 if __name__ == "__main__":
     info = dict(re.findall(r'^__(.*?)__\s*=\s*"(.*?)"', open(sys.argv[1]).read(), re.M))
-    version = tuple((map(int, re.findall(r'\d+', info['version'])) + [0, 0, 0, 0])[:4])
+    version = map(int, re.findall(r'\d+', info['version']))[:3]
+    assert len(version) == 3
+
+    svn = 0
+    try:
+        svn_pid = subprocess.Popen(["svn", "info"], stdout=subprocess.PIPE)
+        versions = re.findall(r'^(revision|last changed rev.*?)\s*:\s*(\d+)', svn_pid.stdout.read(), re.I + re.M)
+        svn_pid.wait()
+        if versions:
+            svn = max(map(int, (v[1] for v in versions)))
+    except:
+        print >>sys.stderr, "Failed to get SVN revision!"
+    fullversion = info['version']
+    if svn:
+        fullversion += " (SVN r%d)" % svn
+
     print "VSVersionInfo("
     print "  ffi=FixedFileInfo("
-    print "    filevers=%r," % (version,)
-    print "    prodvers=%r," % (version,)
+    print "    filevers=%r," % (tuple(version + [svn]),)
+    print "    prodvers=%r," % (tuple(version + [0]),)
     print "    mask=0x0,"
     print "    flags=0x0,"
     print "    OS=0x4,"
@@ -23,7 +38,7 @@ if __name__ == "__main__":
     print "        u'000004b0',"
     print "        [StringStruct(u'CompanyName', %r)," % unicode(info['author'])
     print "        StringStruct(u'FileDescription', %r)," % unicode(info['title'])
-    print "        StringStruct(u'FileVersion', %r)," % unicode(info['version'])
+    print "        StringStruct(u'FileVersion', %r)," % unicode(fullversion)
     print "        StringStruct(u'OriginalFilename', %r)," % unicode(sys.argv[1])
     print "        StringStruct(u'ProductName', %r)," % unicode(info['title'])
     print "        StringStruct(u'ProductVersion', %r)])" % unicode(info['version'])
