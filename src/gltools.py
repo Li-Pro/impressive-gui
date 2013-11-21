3892aa84ae90b0161f4fa37d035fba90ab00e05d
keyj <keyj@58003caa-664d-4c56-a378-6d187ff66ee1>
2013-11-21 21:10:32 +0000

    increased default spotlight tesselation a bit
    git-svn-id: https://svn.emphy.de/impressive/trunk/impressive@82 58003caa-664d-4c56-a378-6d187ff66ee1

--- src/gltools.py
+++ src/gltools.py
@@ -64,6 +64,6 @@ def GenerateSpotMesh():
     ry0 = SpotRadius * PixelY
     rx1 = (SpotRadius + BoxEdgeSize) * PixelX
     ry1 = (SpotRadius + BoxEdgeSize) * PixelY
-    steps = max(6, int(2.0 * pi * SpotRadius / SpotDetail / ZoomArea))
+    steps = max(MinSpotDetail, int(2.0 * pi * SpotRadius / SpotDetail / ZoomArea))
     SpotMesh=[(rx0 * sin(a), ry0 * cos(a), rx1 * sin(a), ry1 * cos(a)) for a in \
              [i * 2.0 * pi / steps for i in range(steps + 1)]]
