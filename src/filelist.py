596784214eeed8315e6d051f61f6c713ab6ec310
keyj <keyj@58003caa-664d-4c56-a378-6d187ff66ee1>
2013-11-25 17:17:49 +0000

    documented the working directory changes introduced in r90; also fixed the file list generator such that the rules for building .info file names actually work as documented again
    git-svn-id: https://svn.emphy.de/impressive/trunk/impressive@92 58003caa-664d-4c56-a378-6d187ff66ee1

--- src/filelist.py
+++ src/filelist.py
@@ -6,10 +6,21 @@ def IsImageFileName(name):
 def IsPlayable(name):
     return IsImageFileName(name) or name.lower().endswith(".pdf") or os.path.isdir(name)
 
-def AddFile(name, title=None):
+def AddFile(name, title=None, implicit=False):
     global FileList, FileName
 
-    name = os.path.normpath(os.path.abspath(name))
+    # generate absolute path
+    path_sep_at_end = name.endswith(os.path.sep)
+    name = os.path.normpath(os.path.abspath(name)).rstrip(os.path.sep)
+    if path_sep_at_end:
+        name += os.path.sep
+
+    # set FileName to first (explicitly specified) input file
+    if not implicit:
+        if not FileList:
+            FileName = name
+        else:
+            FileName = ""
 
     if os.path.isfile(name):
         FileList.append(name)
@@ -20,7 +31,8 @@ def AddFile(name, title=None):
         images.sort(lambda a, b: cmp(a.lower(), b.lower()))
         if not images:
             print >>sys.stderr, "Warning: no image files in directory `%s'" % name
-        for img in images: AddFile(img)
+        for img in images:
+            AddFile(img, implicit=True)
 
     elif name.startswith('@') and os.path.isfile(name[1:]):
         name = name[1:]
@@ -36,18 +48,14 @@ def AddFile(name, title=None):
                 else:
                     subfile, title = line
                 if subfile:
-                    AddFile(os.path.normpath(os.path.join(dirname, subfile)), title)
+                    AddFile(os.path.normpath(os.path.join(dirname, subfile)), title, implicit=True)
             f.close()
         except IOError:
             print >>sys.stderr, "Error: cannot read list file `%s'" % name
-        if not FileName:
-            FileName = name
-        else:
-            FileName = ""
 
     else:
         files = list(filter(IsPlayable, glob.glob(name)))
         if files:
-            for f in files: AddFile(f)
+            for f in files: AddFile(f, implicit=True)
         else:
             print >>sys.stderr, "Error: input file `%s' not found" % name
