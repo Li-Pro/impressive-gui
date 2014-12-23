6a5db1d58a78945f7b88bceb2df53ca55fdac0fe
keyj <keyj@58003caa-664d-4c56-a378-6d187ff66ee1>
2014-12-23 10:34:58 +0000

    fixed SDL library lookup issue on newer POSIX systems
    git-svn-id: https://svn.emphy.de/impressive/trunk/impressive@173 58003caa-664d-4c56-a378-6d187ff66ee1

--- src/platform.py
+++ src/platform.py
@@ -43,7 +43,7 @@ class Platform_PyGame(object):
 
     def LoadOpenGL(self):
         try:
-            sdl = CDLL(ctypes.util.find_library("SDL") or "SDL", RTLD_GLOBAL)
+            sdl = CDLL(ctypes.util.find_library("SDL") or ctypes.util.find_library("SDL-1.2") or "SDL", RTLD_GLOBAL)
             get_proc_address = CFUNCTYPE(c_void_p, c_char_p)(('SDL_GL_GetProcAddress', sdl))
         except OSError:
             raise ImportError("failed to load the SDL library")
